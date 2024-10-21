d7ed662dd118ee980d53e0a713c92c72ed523d1b
mouazSeiam <130285760+mouazSeiam@users.noreply.github.com>
2024-10-21 17:26:06 +0300

    Update BankAccount.cs
    no thing i just learning

--- 01.DefiningClasses-Lab/02.BankAccountMethods/BankAccount.cs
+++ 01.DefiningClasses-Lab/02.BankAccountMethods/BankAccount.cs
@@ -3,30 +3,13 @@
     private int id;
     private decimal balance;
 
-    public decimal Balance
-    {
-        get { return this.balance; }
-        set { this.balance = value; }
-    }
+    public decimal Balance { get; set;}
 
-    public int Id
-    {
-        get { return this.id; }
-        set { this.id = value; }
-    }
+    public int Id { get; set;}
 
-    public void Deposit(decimal amount)
-    {
-        this.Balance += amount;
-    }
+    public void Deposit(decimal amount) => Balance += amount;
 
-    public void Withdraw(decimal amount)
-    {
-        this.Balance -= amount;
-    }
-
-    public override string ToString()
-    {
-        return $"Account {this.Id}, balance {this.Balance}";
-    }
+    public void Withdraw(decimal amount) => Balance -= amount;
+     
+    public override string ToString() => $"Acount{Id}, balance {Balance}";
 }
